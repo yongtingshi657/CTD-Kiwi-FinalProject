27773a4447f8e86a6d4fa6e0d72fd3cc1d800fa7
Yongting <yongtingshi657@gmail.com>
2025-10-15 23:16:22 -0400

    env local added

--- firebase.js
+++ firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8QwH6zbgVWksFEsipcqeiKBLHuvPAsoE",
-  authDomain: "mygoodbuy-3d398.firebaseapp.com",
-  projectId: "mygoodbuy-3d398",
-  storageBucket: "mygoodbuy-3d398.firebasestorage.app",
-  messagingSenderId: "564372990453",
-  appId: "1:564372990453:web:8927890c81a401fe9958bc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 
